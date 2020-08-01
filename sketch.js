b9d1b0f4dad6f7cf12c9bc3ab07f89354f91d03e
Ayush111200 <64826573+Ayush111200@users.noreply.github.com>
2020-08-01 13:47:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var happyDog, database, foodS, foodStock;
+var dog = createSprite(100,100);
+this.image = loadImage("doglmg");
+
+function preload()
+{
+  //load images here
+  this.image = loadImage("doglmg");
+  this.image = loadImage("dogimg.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
